885b420599ba0d07aa1f87259b31c59a84d73cc6
Abdumannof <abduvalievabap@gmail.com>
2026-01-19 17:31:39 +0500

    Home page and NavBar has been updated

---
+++ scripts/components/menu.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const openNavMenu = document.getElementById("nav_menu");
+    const closeNavMenu = document.getElementById("menu_bar_close");
+    const menuBar = document.getElementById("menu_bar");
+    console.log(openNavMenu);
+
+
+    openNavMenu.addEventListener("click", () => {
+        menuBar.classList.add("active_menu");
+    });
+    closeNavMenu.addEventListener("click", () => {
+        menuBar.classList.remove("active_menu");
+    });
+});
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import "./components/menu.js";
+import "./utils/filter.js";
+import { renderProducts } from './utils/productsRender.js';
+
+
+renderProducts("#productsContainer");
