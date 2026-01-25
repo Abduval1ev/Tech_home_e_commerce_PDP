241ec29babe284a0fb80c6cd5bbe990b37a4892c
Abdumannof <abduvalievabap@gmail.com>
2026-01-25 14:54:29 +0500

    Fix: Bottom bar

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,11 @@ const button = document.getElementById("myButton");
 button.addEventListener("click", () => {
     location.href = location.href;
 });
+
+const navMenu = document.getElementById('nav_menu');
+const mobileMenu = document.getElementById('mobile_menu');
+
+navMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    mobileMenu.classList.toggle('show');
+});
